9bbb15b44bd92448e28ef3bf809664e8a061afea
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2014-04-30 15:41:40 +0200

    RESEARCH-87: Object command api test

--- Tests/Fixtures/TestProject/WebTestCase.php
+++ Tests/Fixtures/TestProject/WebTestCase.php
@@ -2,9 +2,27 @@
 namespace Imatic\Bundle\ControllerBundle\Tests\Fixtures\TestProject;
 
 use Imatic\Bundle\TestingBundle\Test\WebTestCase as BaseWebTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Doctrine\ORM\EntityManager;
 
 class WebTestCase extends BaseWebTestCase
 {
+    /** @var ContainerInterface */
+    protected $container;
+
+    protected function setUp()
+    {
+        $this->container = static::createClient()->getContainer();
+    }
+
+    /**
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->container->get('doctrine.orm.entity_manager');
+    }
+
     /**
      * @return string
      */
