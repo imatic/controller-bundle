1a9ac18b887641ab7010bc4fc33da793d0bbfd1b
Štěpán Kočí <stepan.koci@imatic.cz>
2017-02-07 23:07:54 +0100

    Resource: non ajax delete controller, ajaxify support.

--- Controller/Resource/DeleteController.php
+++ Controller/Resource/DeleteController.php
@@ -3,6 +3,8 @@
 namespace Imatic\Bundle\ControllerBundle\Controller\Resource;
 
 use Imatic\Bundle\ControllerBundle\Controller\Api\Command\CommandApi;
+use Imatic\Bundle\ControllerBundle\Controller\Api\Show\ShowApi;
+use Symfony\Component\HttpFoundation\Request;
 
 class DeleteController extends ResourceController
 {
@@ -14,18 +16,35 @@ class DeleteController extends ResourceController
         return $this->getApi('imatic_controller.api.command', 'command', func_get_args());
     }
 
-    public function deleteAction($id)
+    /**
+     * @return ShowApi
+     */
+    public function show()
+    {
+        return $this->getApi('imatic_controller.api.show', 'show', func_get_args());
+    }
+
+    public function deleteAction(Request $request, $id)
     {
         $config = $this->getActionConfig();
-        $commandParameters = [
-            'item' => $id,
-            'class' => $config['entity'],
-            'query_object' => new $config['query']($id, $config['entity']),
-        ];
 
-        return $this
-            ->command($config['command'], $commandParameters)
-            ->redirect($config['redirect'])
-            ->getResponse();
+        if ($request->isMethod('delete')) {
+            $commandParameters = [
+                'item' => $id,
+                'class' => $config['entity'],
+                'query_object' => new $config['query']($id, $config['entity']),
+            ];
+
+            return $this
+                ->command($config['command'], $commandParameters)
+                ->redirect($config['redirect'])
+                ->getResponse();
+        } else {
+            return $this->show(new $config['query']($id, $config['entity']))
+                ->setTemplateName($config['template'])
+                ->addTemplateVariable('action', $config)
+                ->addTemplateVariable('resource', $this->getResourceConfig())
+                ->getResponse();
+        }
     }
 }
